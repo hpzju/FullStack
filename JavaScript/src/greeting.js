d6a3c83c00c296c087741f40268fdb896f4606ac
hpzju <hpzjuplus@gmail.com>
2019-05-28 13:43:40 +0800

    udpate JS

---
+++ JavaScript/src/greeting.js
@@ -0,0 +1,5 @@
+const greeting = msg => {
+  console.log(`greeting from ${__file__} with ${msg}!!`);
+};
+
+export default greeting;
---
+++ JavaScript/src/index.js
@@ -0,0 +1,4 @@
+import greeting from "./greeting";
+console.log("Hello index.js with Webpack");
+
+greeting("call from index.js");
