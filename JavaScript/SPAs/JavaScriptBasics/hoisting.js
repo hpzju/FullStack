298be0f48f5b427722f982e4fa1643c03167c090
hpzju <hpzjuplus@gmail.com>
2019-05-30 08:07:23 +0800

    update JS

---
+++ JavaScript/SPAs/JavaScriptBasics/hoisting.js
@@ -0,0 +1,27 @@
+// let----------------------------------
+// a = 10; //Cannot access 'a' before initialization
+
+// console.log(a);
+
+// let a;
+
+// console.log(a);
+
+// const----------------------------------
+
+// a = 10;
+
+// console.log(a);
+
+// const a; //Missing initializer in const declaration
+
+// console.log(a);
+
+// var----------------------------------
+a = 10;
+
+console.log(a); //10
+
+var a;
+
+console.log(a); //10
