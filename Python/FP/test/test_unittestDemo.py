a557b7f882c8137e9fd9c25a5602a664c5ed3e56
hpzju <hpzjuplus@gmail.com>
2019-07-04 22:05:42 +0800

    update

--- Python/FP/test/test_unittestDemo.py
+++ Python/FP/test/test_unittestDemo.py
@@ -4,19 +4,24 @@ from unittestDemo import adder, adder10, Pet
 
 class unittestDemoTester(unittest.TestCase):
     def test_adder(self):
+        """testing adder()"""
         self.assertEqual(adder(1, 5), 6)
         self.assertNotEqual(adder(10, 10), 10)
 
     def test_adder10(self):
+        """testing adder10()"""
         self.assertEqual(adder10(5), 15)
         self.assertEqual(adder10(5), adder(5, 10))
         self.assertNotEqual(adder10(20), 15)
 
     def test_Pet(self):
+        """testing Pet Class"""
         cat = Pet("cat")
         dog = Pet("dog")
         self.assertEqual(cat.name, "cat")
-        self.assertNotEqual(dog.name, "cat")
+        self.assertEqual(dog.name, "dog")
+        dog.name = "DDDDog"
+        self.assertEqual(dog.name, "DDDDog")
         self.assertEqual(Pet.counts(), 2)
 
 
